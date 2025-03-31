d9d155d94a394891fdf103575db59b4650d92101
GsLee <lgswin@gmail.com>
2025-03-31 01:48:12 -0400

    add sql file to migrate

---
+++ migrations/V1__create_table.sql
@@ -0,0 +1,8 @@
+-- V1__create_table.sql
+
+CREATE TABLE IF NOT EXISTS subscribers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
